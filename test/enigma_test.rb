698dc7f3ce48b83f6db61e344aa3319e9b798e38
“Neeru <neeram85@gmail.com>
2020-06-03 15:53:46 -0600

    Test it exists

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,7 +1,16 @@
+require 'simplecov'
+SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/enigma'
 require 'pry'
 
 class EnigmaTest < Minitest::Test
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
 end
