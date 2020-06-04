7efca403341c713350dec5e93f138c43ac7aa4a8
“Neeru <neeram85@gmail.com>
2020-06-03 19:10:35 -0600

    Randomize Key method complete with passing test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,2 +1,22 @@
+require 'date'
 class Enigma
+  attr_reader :numbers,
+              :alphabet,
+              :key
+  def initialize
+    @numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    @alphabet = ("a".."z").to_a << " "
+    @key = []
+  end
+
+  def encrypt(message, key = randomize_key, date)
+    {encryption: message, key: key, date: date}
+  end
+
+  def randomize_key
+    5.times do
+      @key << @numbers.sample
+    end
+    @key.join
+  end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -2,6 +2,7 @@ require 'simplecov'
 SimpleCov.start
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 require './lib/enigma'
 require 'pry'
 
@@ -12,5 +13,28 @@ class EnigmaTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Enigma, @enigma
+  end
+
+  def test_it_has_attributes
+    expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+    expected1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"," "]
+    assert_equal expected, @enigma.numbers
+    assert_equal expected1, @enigma.alphabet
+    assert_equal [], @enigma.key
+  end
+
+  def test_it_can_encrypt
+    expected = {
+              encryption: "keder ohulw",
+              key: "02715",
+              date: "040895"
+    }
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_it_can_randomize_key
+    @enigma.stubs(:randomize_key).returns("34573")
+
+    assert_equal "34573", @enigma.encrypt("hello world", "040895")[:key]
   end
 end
