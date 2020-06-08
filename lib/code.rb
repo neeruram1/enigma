5adaf623872d0dc7d4b3ac83475c4c170a2301c0
“Neeru <neeram85@gmail.com>
2020-06-08 09:34:51 -0600

    Moved functionality from Code class to Encryption and Decryption classes

--- lib/code.rb
+++ lib/code.rb
@@ -41,46 +41,4 @@ class Code
   def included_in_d?(character, position)
     @alphabet.include?(character) && position % 4 == 0
   end
-
-  def rotate_message_encode
-    ashift = @shift.shift_values[:A]
-    bshift = @shift.shift_values[:B]
-    cshift = @shift.shift_values[:C]
-    dshift = @shift.shift_values[:D]
-
-    message_position = start_positions
-    message_position.map do |position, character|
-      if included_in_a?(character, position)
-        message_position[position] = shifted_alphabet(ashift)[character]
-      elsif included_in_b?(character, position)
-        message_position[position] = shifted_alphabet(bshift)[character]
-      elsif included_in_c?(character, position)
-        message_position[position] = shifted_alphabet(cshift)[character]
-      elsif included_in_d?(character, position)
-        message_position[position] = shifted_alphabet(dshift)[character]
-      end
-    end
-    message_position.values.join
-  end
-
-  def rotate_message_decode
-    ashift = @shift.shift_values[:A] * -1
-    bshift = @shift.shift_values[:B] * -1
-    cshift = @shift.shift_values[:C] * -1
-    dshift = @shift.shift_values[:D] * -1
-
-    message_position = start_positions
-    message_position.map do |position, character|
-      if included_in_a?(character, position)
-        message_position[position] = shifted_alphabet(ashift)[character]
-      elsif included_in_b?(character, position)
-        message_position[position] = shifted_alphabet(bshift)[character]
-      elsif included_in_c?(character, position)
-        message_position[position] = shifted_alphabet(cshift)[character]
-      elsif included_in_d?(character, position)
-        message_position[position] = shifted_alphabet(dshift)[character]
-      end
-    end
-    message_position.values.join
-  end
 end
--- lib/decryption.rb
+++ lib/decryption.rb
@@ -3,6 +3,22 @@ require_relative 'code'
 class Decryption < Code
 
   def decode
-    rotate_message_decode
+    ashift = @shift.shift_values[:A] * -1
+    bshift = @shift.shift_values[:B] * -1
+    cshift = @shift.shift_values[:C] * -1
+    dshift = @shift.shift_values[:D] * -1
+    message_position = start_positions
+    message_position.map do |position, character|
+      if included_in_a?(character, position)
+        message_position[position] = shifted_alphabet(ashift)[character]
+    elsif included_in_b?(character, position)
+        message_position[position] = shifted_alphabet(bshift)[character]
+      elsif included_in_c?(character, position)
+      message_position[position] = shifted_alphabet(cshift)[character]
+      elsif included_in_d?(character, position)
+        message_position[position] = shifted_alphabet(dshift)[character]
+      end
+    end
+    message_position.values.join
   end
 end
