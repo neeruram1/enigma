9530a9cd18fc7b12e5b447b3f74fc18f997f5721
“Neeru <neeram85@gmail.com>
2020-06-07 18:41:34 -0600

    Starting to refactor Enigma class

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -7,60 +7,12 @@ require_relative 'decryption'
 require_relative 'shift'
 
 class Enigma
-  attr_reader :numbers,
-              :alphabet,
-              :key,
-              :date,
-              :message,
-              :shift
 
 
-  def encrypt(message, key = Key.new.randomize_key, date = Offset.new.date)
-    @message = message
-    @key = key
-    @date = date
-    @shift = Shift.new(@key, @date)
-    {encryption: encode, key: @key, date: @date}
-    binding.pry
+  def encrypt
   end
 
-  def decrypt(message, key, date = create_date)
-    @message = message
-    @key = key
-    @date = date
-    {decryption: decode, key: @key, date: @date}
+  def decrypt
   end
 
-#MAKE A MODULE
-  def create_date
-    Date.today.strftime("%d%m%y")
-  end
-
-  def decode
-    a_rotated_alphabet = @alphabet.rotate(@shift.shift_values[:A] * -1)
-    a_shift_alpha = Hash[@alphabet.zip a_rotated_alphabet]
-
-    b_rotated_alphabet = @alphabet.rotate(@shift.shift_values[:B] * -1)
-    b_shift_alpha = Hash[@alphabet.zip b_rotated_alphabet]
-
-    c_rotated_alphabet = @alphabet.rotate(@shift.shift_values[:C] * -1)
-    c_shift_alpha = Hash[@alphabet.zip c_rotated_alphabet]
-
-    d_rotated_alphabet = @alphabet.rotate(@shift.shift_values[:D] * -1)
-    d_shift_alpha = Hash[@alphabet.zip d_rotated_alphabet]
-
-    message_chars = start_positions
-    message_chars.map do |position, character|
-      if @alphabet.include?(message_chars[position]) && position % 4 == 1
-        message_chars[position] = a_shift_alpha[character]
-      elsif @alphabet.include?(message_chars[position]) && position % 4 == 2
-        message_chars[position] = b_shift_alpha[character]
-      elsif @alphabet.include?(message_chars[position]) && position % 4 == 3
-        message_chars[position] = c_shift_alpha[character]
-      elsif @alphabet.include?(message_chars[position]) && position % 4 == 0
-        message_chars[position] = d_shift_alpha[character]
-      end
-    end
-    message_chars.values.join.chomp
-  end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -6,56 +6,14 @@ require './lib/enigma'
 require 'pry'
 
 class EnigmaTest < Minitest::Test
+  def setup
+
+
+  end
   def test_it_exists
     enigma = Enigma.new
     assert_instance_of Enigma, enigma
   end
 
-  def test_it_has_attributes
-    skip
-    enigma = Enigma.new
-    expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-    expected1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"," "]
-    assert_equal expected, enigma.numbers
-    assert_equal expected1, enigma.alphabet
-  end
 
-  def test_it_can_encrypt
-    skip
-    enigma = Enigma.new
-    expected = {
-              encryption: "keder ohulw",
-              key: "02715",
-              date: "040895"
-            }
-    assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
-  end
-
-  def test_it_can_decrypt
-    skip
-    enigma = Enigma.new
-    expected = {
-              decryption: "hello world",
-              key: "02715",
-              date: "040895"
-            }
-    assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
-  end
-
-  def test_it_has_default_date
-    skip
-    enigma = Enigma.new
-    enigma.stubs(:create_date).returns("030620")
-    enigma.encrypt("hello world")
-
-    assert_equal "030620", enigma.date
-  end
-
-  def test_it_can_decode
-    skip
-    enigma = Enigma.new
-    enigma.encrypt("keder ohulw!", "02715", "040895")
-    expected = "hello world!"
-    assert_equal expected, enigma.decode
-  end
 end
